e03338cce099fc6386e3a09a39c58c24d86f24c1
Keren-Happuc Fleurimond <keren@noukod.com>
2018-09-03 16:02:03 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -14,9 +14,6 @@ def turn_count(turn)
     if unit == "X" || unit == "O"
       #puts "spot taken"
       move_count.push(1)
-    #else
-      #puts "spot free"
-      #counter += 1
     end
   end
   move_count.length.to_i
